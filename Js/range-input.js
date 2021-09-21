19b72a63bc7959298918d0e31247c87e58747cc3
arash esmailian <arash.esmailian@gmail.com>
2021-09-21 23:27:10 +0430

    First commit

---
+++ Js/range-input.js
@@ -0,0 +1,9 @@
+const input = document.getElementById("range") ;
+const min = input.min ;
+const max = input.max ;
+const value = input.value ;
+
+input.style.background = 'linear-gradient(to right, red 0%, red ${(value-min)/(max-min)*100}%, blue &{(value-min)/(max-min)*100}%, blue 100%)'
+input.oninput = function () {
+  this.style.background = 'linear-gradient(to right, red 0%, red ${(this.value-this.min)/(this.max-this.min)*100}%, blue &{(this.value-this.min)/(this.max-this.min)*100}%, blue 100%)'
+};
